fdbed8ecefedf11e618fc2c8889dd4a0b979bd57
Ludovic PETRENKO <ludovic.petrenko@epitech.eu>
2019-09-20 00:59:25 +0200

    Started main page prototype with multilang support

---
+++ src/components/Introduction.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  .background
+    .title LUDOVIC PETRENKO
+    h2 Full Stack Software Engineer
+    .code-link Github
+    .code-link Gitlab
+</template>
+
+<style lang="scss" scoped>
+.background {
+  width: 100%;
+  height: 900px;
+  background: black;
+  color: white;
+  display: grid;
+  align-content: center;
+  justify-content: center;
+}
+
+.title {
+  font-family: 'Roboto';
+  font-weight: 400;
+  font-size: 90px;
+  margin: 20px 0;
+}
+
+h2 {
+  font-family: 'Roboto';
+  font-size: 40px;
+  font-weight: 200;
+  text-align: center;
+}
+</style>
---
+++ src/components/MenuBar.vue
@@ -0,0 +1,63 @@
+<template lang="pug">
+  div
+    .main-menu
+      .home-button Ludovic Petrenko
+      div
+      .menu-buttons
+        .menu-button(v-for="(opt, i) of menuOptions" :key="i")
+          router-link.links(:to="opt.link") {{ opt.label }}
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class MenuBar extends Vue {
+  private data() {
+    return {
+      menuOptions: [
+        {
+          label: this.$t('menu.description'),
+          link: '#description',
+        },
+        {
+          label: this.$t('menu.experiences'),
+          link: '#experiences',
+        },
+        {
+          label: this.$t('menu.projects'),
+          link: '#projects',
+        },
+        {
+          label: this.$t('menu.contacts'),
+          link: '#contacts',
+        },
+      ],
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+
+.main-menu {
+  color: white;
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  padding: 20px;
+}
+
+.menu-buttons {
+  display: grid;
+  grid-template-columns: repeat(4, auto);
+  grid-column-gap: 40px;
+  margin-right: 100px;
+}
+
+.links {
+  color: white;
+  text-decoration: none;
+}
+</style>
