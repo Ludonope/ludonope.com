c2708df8eb9ce5d360a0f1b5ecdf93f37b4548af
Ludovic PETRENKO <ludovic.petrenko@epitech.eu>
2019-09-25 22:15:59 +0200

    Allowed to navigate to anchor

--- src/router.ts
+++ src/router.ts
@@ -19,5 +19,15 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    //https://router.vuejs.org/guide/advanced/scroll-behavior.html
+    if (to.hash) {
+      return { selector: to.hash }
+    } else if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
