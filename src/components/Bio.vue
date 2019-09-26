dd6522f79c2b61033844ee3301120f91591c01dc
Ludovic PETRENKO <ludovic.petrenko@epitech.eu>
2019-09-26 14:01:36 +0200

    Added bio section

---
+++ src/components/Bio.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+  .bio-section
+    div(v-observe-visibility="visibilityChanged")
+    transition(name="fade")
+      .text(v-show="!delay") {{ $t('intro.bio') }}
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'Bio'
+})
+export default class Bio extends Vue {
+  delay: number
+
+  constructor () {
+    super()
+    this.delay = 200
+  }
+
+  visibilityChanged (isVisible: boolean, entry: any) {
+    this.delay = 200
+
+    if (isVisible) {
+      setTimeout(() => {
+        this.delay = 0
+      }, this.delay)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.bio-section {
+  color: white;
+  width: 100%;
+  min-height: 110vh;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+}
+
+.text {
+  max-width: 600px;
+  text-align: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template lang="pug">
   .home
     Introduction.section#intro
+    Bio
     Projects.section#projects
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import Introduction from '@/components/Introduction.vue'
+import Bio from '@/components/Bio.vue'
 import Projects from '@/components/Projects.vue'
 
 @Component({
   components: {
     Introduction,
+    Bio,
     Projects
   }
 })
